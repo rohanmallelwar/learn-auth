53e14754df7a363dfb37abf8dcf1e97f8aeb3542
Rohan MALLELWAR (contractor) <rohan.mallelwar@amadeus.com>
2017-01-11 12:11:55 +0530

    Adding Auth and AuthGuard

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {Auth} from './services/auth.service';
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  constructor(private auth: Auth){
+
+  }
+
+}
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -3,6 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 
 import {HomeComponent} from './components/home/home.component';
 import {ProfileComponent} from './components/profile/profile.component';
+import {AuthGuard} from './auth.guard';
 
 const appRoutes: Routes =[
     {
@@ -11,7 +12,8 @@ const appRoutes: Routes =[
     },
     {
         path : 'profile',
-        component : ProfileComponent
+        component : ProfileComponent,
+        canActivate : [AuthGuard]
     }
 ];
 
